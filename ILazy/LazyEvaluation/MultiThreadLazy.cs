4d0d8278bd13bef07590ba148f43d95234cfb2e0
Alehandro Krava <115242003+AwIiwiIwA@users.noreply.github.com>
2024-09-26 17:54:27 +0300

    HW-2 ILazy

---
+++ ILazy/LazyEvaluation/MultiThreadLazy.cs
@@ -0,0 +1,29 @@
+﻿public class MultiThreadLazy<T> : ILazy<T>
+{
+    private Func<T> supplier;
+    private T value;
+    private bool isEvaluated;
+    private readonly object lockObject = new object();
+
+    public MultiThreadLazy(Func<T> supplier)
+    {
+        this.supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
+    }
+
+    public T Get()
+    {
+        if (!isEvaluated)
+        {
+            lock (lockObject)
+            {
+                if (!isEvaluated)
+                {
+                    value = supplier();
+                    supplier = null; // Освобождаем supplier, так как больше не нужно
+                    isEvaluated = true;
+                }
+            }
+        }
+        return value;
+    }
+}
