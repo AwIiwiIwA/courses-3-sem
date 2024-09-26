da04df123b10f21432b784761d54c08376711125
Alehandro Krava <115242003+AwIiwiIwA@users.noreply.github.com>
2024-09-26 17:02:38 +0300

    Add files via upload

---
+++ Lazy/LazyEvaluation.Tests/MultiThreadLazyTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+
+[TestFixture]
+public class MultiThreadLazyTests
+{
+    [Test]
+    public void ShouldReturnSameValueOnMultipleCalls()
+    {
+        var lazy = new MultiThreadLazy<int>(() => 42);
+        Assert.AreEqual(42, lazy.Get());
+        Assert.AreEqual(42, lazy.Get());
+    }
+
+    [Test]
+    public void ShouldEvaluateOnlyOnceInMultithreadedEnvironment()
+    {
+        int counter = 0;
+        var lazy = new MultiThreadLazy<int>(() => { counter++; return 42; });
+
+        Parallel.Invoke(
+            () => Assert.AreEqual(42, lazy.Get()),
+            () => Assert.AreEqual(42, lazy.Get())
+        );
+
+        Assert.AreEqual(1, counter);
+    }
+
+    [Test]
+    public void ShouldSupportNullValueInMultithreadedEnvironment()
+    {
+        var lazy = new MultiThreadLazy<string>(() => null);
+
+        Parallel.Invoke(
+            () => Assert.IsNull(lazy.Get()),
+            () => Assert.IsNull(lazy.Get())
+        );
+    }
+}
---
+++ Lazy/LazyEvaluation/SingleThreadLazy.cs
@@ -0,0 +1,22 @@
+﻿public class SingleThreadLazy<T> : ILazy<T>
+{
+    private Func<T> supplier;
+    private T value;
+    private bool isEvaluated;
+
+    public SingleThreadLazy(Func<T> supplier)
+    {
+        this.supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
+    }
+
+    public T Get()
+    {
+        if (!isEvaluated)
+        {
+            value = supplier();
+            supplier = null; // Освобождаем supplier, так как больше не нужно
+            isEvaluated = true;
+        }
+        return value;
+    }
+}
